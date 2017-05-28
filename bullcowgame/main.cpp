ad08e4d959e769ca17ac6f3912c4abca2cf955d8
Gerard <gdavid32@hotmail.com>
2017-05-28 19:05:55 -0300

    Better output form look of the game.

--- bullcowgame/main.cpp
+++ bullcowgame/main.cpp
@@ -8,18 +8,24 @@ void PrintGuessMessage(std::string);
 void PlayGame();
 bool AskToPLayAgain();
 FBullCowGame BCGame;
+const int NUMBER_OF_TURNS = 5;
 
-int main() {
-	
+
+int main() {	
+
 	bool bPlayAgain = false;
 
-	do {// Print welcome message 
+	do {
+		// Print welcome message 
 		PrintIntro();
+
 		// Starts the gane 
 		PlayGame();
+
 		// Asks player if they choose to play again
-		bool bPlayAgain = AskToPLayAgain();
-	} while (bPlayAgain);
+		bPlayAgain = AskToPLayAgain();
+
+	} while (bPlayAgain);	
 
 	return 0; // Exits game program.
 }
@@ -52,29 +58,30 @@ void PrintGuessMessage(std::string Message) {
 }
 
 void PlayGame() {	
-	// Sets the number of guesses the player gets
-	constexpr int NUMBER_OF_TURNS = 5;
+
+	// Sets the number of guesses the player gets	
 	int Max_turns = BCGame.GetMaxTries();
 	std::cout << "Your number of guesses: " << Max_turns << std::endl;
-
-
+	
 	// Loop through by alotted turns and get and print guess to console
 	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
 		PrintGuessMessage(GetGuess());
 		int Tries_left = BCGame.GetRemainingTries();
-		std::cout << "Your number of remaining guesses: " << Tries_left << std::endl;
-	}
-	std::cout << std::endl;
+		std::cout << "Your number of remaining guesses: " << Tries_left << std::endl;		
+	}		
 }
 
 bool AskToPLayAgain() {
-	// Gets player response and inputs from user entry
+
 	std::string Response = "";
-	std::cout << "Do you want to play again? (y/n).\n";
-	std::getline(std::cin, Response);
-	
+	// Gets player response and inputs from user entry	
+	std::cout << "\nDo you want to play again? (y/n) ";		
+	std::getline(std::cin, Response);	
+
 	// Checks the first char in the Response std::string and then returns true or false if not found		
 	if (Response[0] == 'y' || Response[0] == 'Y') {
+
+		system("cls");
 		return true;
 	}
 	else {
